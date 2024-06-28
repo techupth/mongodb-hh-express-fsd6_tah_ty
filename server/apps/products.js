1ed7ac0d3c82e16bfc2b098c7a654a17e8370908
TiTY <tee25450@gmail.com>
2024-06-28 15:11:24 +0700

    creating APIs on Express with MongoDB

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,59 @@
 import { Router } from "express";
-
+import { ObjectId } from "mongodb";
+import { db } from "../utils/db.js";
+import express from "express";
 const productRouter = Router();
+const app = express();
 
-productRouter.get("/", (req, res) => {});
+app.use(express.json());
+productRouter.get("/", async (req, res) => {
+  const collection = db.collection("products");
+  const products = await collection.find({}).toArray();
+  return res.json({ data: products });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const product = await collection.findOne({ _id: productId });
+  return res.json({ data: product });
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const collection = db.collection("products");
+  const productData = { ...req.body };
+  await collection.insertOne(productData);
+  return res.json({
+    message: "Product has been created successfully",
+  });
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  const newProductData = { ...req.body };
+  await collection.updateOne(
+    {
+      _id: productId,
+    },
+    {
+      $set: newProductData,
+    }
+  );
+  return res.json({
+    message: "Product has been updated successfully",
+  });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.delete("/:id", async (req, res) => {
+  const collection = db.collection("products");
+  const productId = new ObjectId(req.params.id);
+  await collection.deleteOne({
+    _id: productId,
+  });
+  return res.json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
